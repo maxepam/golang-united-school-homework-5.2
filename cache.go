02bf20660aaab52ea5b5836d0259f0cbeedeb533
Max Gor <m.hornostaiev@myfeelix.de>
2022-05-26 17:25:16 +0300

    added removing element from map when element is expired

--- cache.go
+++ cache.go
@@ -26,9 +26,13 @@ func newValue(value string, isTimeout bool, deadline time.Time) cacheValue {
 	return cacheValue{value: value, isTimeout: isTimeout, deadline: deadline}
 }
 
+func isExpired(deadline time.Time) bool {
+	return deadline.Before(time.Now())
+}
+
 func (c Cache) Get(key string) (string, bool) {
 	if val, ok := c.values[key]; ok {
-		if val.isTimeout && val.deadline.Before(time.Now())  {
+		if val.isTimeout && isExpired(val.deadline)  {
 			return "", false
 		}
 		return val.value, true
@@ -40,11 +44,13 @@ func (c *Cache) Put(key, value string) {
 	c.values[key] = newValue(value, false, time.Now())
 }
 
-func (c Cache) Keys() []string {
+func (c *Cache) Keys() []string {
 	ret := make([]string, len(c.values))
 	for k, val := range c.values {
-		if !val.isTimeout || (val.isTimeout && val.deadline.After(time.Now())) {
+		if !val.isTimeout || (val.isTimeout && !isExpired(val.deadline)) {
 			ret = append(ret, k)
+		} else {
+			delete(c.values, k)
 		}
 		
 	  }
@@ -60,13 +66,13 @@ func (c *Cache) PutTill(key, value string, deadline time.Time) {
 // 	fmt.Println(c)
 // 	c.Put("one", "111111")
 // 	c.Put("two", "222222")
-// 	keys := c.Keys()
-// 	fmt.Println(keys)
-// 	t := time.Now().Add(24 * time.Hour)
+// 	t := time.Now().Add(-24 * time.Hour)
 // 	c.PutTill("four", "444444", t)
 // 	newval, err := c.Get("four")
 // 	fmt.Println(newval, err)
 // 	_, err = c.Get("five")
 // 	fmt.Println(err)
+// 	keys := c.Keys()
+// 	fmt.Println(keys)
 // 	fmt.Println(c)
 // }
