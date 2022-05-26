a0f306fa3a33d00031b1181bd5f22b9f879ed374
Max Gor <m.hornostaiev@myfeelix.de>
2022-05-26 17:00:01 +0300

    added homework solution

--- cache.go
+++ cache.go
@@ -1,23 +1,69 @@
 package cache
 
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
 import "time"
 
+type cacheValue struct {
+	value string
+	deadline time.Time
+	isTimeout bool
+}
+
 type Cache struct {
+	values map[string]cacheValue
 }
 
 func NewCache() Cache {
-	return Cache{}
+	values := map[string]cacheValue{}
+	return Cache{values: values}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func newValue(value string, isTimeout bool, deadline time.Time) cacheValue {
+	return cacheValue{value: value, isTimeout: isTimeout, deadline: deadline}
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Get(key string) (string, bool) {
+	if val, ok := c.values[key]; ok {
+		if val.isTimeout && val.deadline.Before(time.Now())  {
+			return "", false
+		}
+		return val.value, true
+	}
+	return "", false
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Put(key, value string) {
+	c.values[key] = newValue(value, false, time.Now())
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) Keys() []string {
+	ret := make([]string, len(c.values))
+	for k := range c.values {
+		ret = append(ret, k)
+	  }
+	  return ret
 }
+
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.values[key] = newValue(value, true, deadline)
+}
+
+// func main() {
+// 	c := NewCache()
+// 	fmt.Println(c)
+// 	c.Put("one", "111111")
+// 	c.Put("two", "222222")
+// 	keys := c.Keys()
+// 	fmt.Println(keys)
+// 	t := time.Now().Add(24 * time.Hour)
+// 	c.PutTill("four", "444444", t)
+// 	newval, err := c.Get("four")
+// 	fmt.Println(newval, err)
+// 	_, err = c.Get("five")
+// 	fmt.Println(err)
+// 	fmt.Println(c)
+// }
